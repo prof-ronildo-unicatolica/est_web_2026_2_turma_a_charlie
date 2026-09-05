df16e7b3d704f29817b20798c8e680b4b39366ed
Pablo Kaiky <pablokjota@gmail.com>
2026-09-05 13:45:13 -0300

    feat: adiciona models de cidade e hotel

---
+++ apps/services/core-service/app/models/hotel.py
@@ -0,0 +1,50 @@
+import uuid
+from typing import List
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.tutorial import Base
+
+
+class Cidade(Base):
+    __tablename__ = "cidades"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    nome: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False,
+    )
+
+    hoteis: Mapped[List["Hotel"]] = relationship(
+        back_populates="cidade",
+        cascade="all, delete-orphan",
+    )
+
+
+class Hotel(Base):
+    __tablename__ = "hoteis"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    nome: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+    )
+
+    cidade_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("cidades.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    cidade: Mapped["Cidade"] = relationship(
+        back_populates="hoteis",
+    )
